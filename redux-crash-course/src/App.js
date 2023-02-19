d48a00d52c3161a3670dbbb4982732f2d4a3b514
Sanket Phatangare <sanket.phatangare.mep@gmail.com>
2023-02-19 22:22:55 +0530

    Created form Submission

---
+++ redux-crash-course/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import PostForm from './Components/PostForm';
+import Posts from './Components/Posts';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <PostForm/>
+      <hr/>
+      <Posts/>
+    </div>
+  );
+}
+
+export default App;
